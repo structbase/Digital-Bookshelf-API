894f45198dd772be390e18e8f4dc5d21e0b08304
Abenezer <abitysank@gmail.com>
2026-01-16 23:38:13 -0500

    Update server.js
    Import the routes into server and integrate it into server

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/connection");
-const PORT = process.env.PORT;
+const bookRoutes = require("./routes/movieRoutes");
+
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Connect to MongoDB
 connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
