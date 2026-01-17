53ae41155c681ad4a8a00eb97d87a8998ae02057
Abenezer <abitysank@gmail.com>
2026-01-16 23:47:58 -0500

    Update connection.js
    Removed options entirely

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGO_URI;
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Successfully connected to MongoDB!");
     } catch (err) {
         console.error("Connection error", err);
